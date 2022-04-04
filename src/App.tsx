12ab96c06e7897d5fec2cf8907015ca64d5536fb
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-04 11:17:38 +0300

    - add props to function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Card from "./components/Card";
+import Card, {CardVariant} from "./components/Card";
 
 const App = () => {
     return (
         <div>
-            <Card width='200px' height="200px"/>
+            <Card onClick={(num)=> console.log('Props', num)} width='200px' height="200px" variant={CardVariant.primary}>
+                <button>Button</button>
+            </Card>
         </div>
     );
 };
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+export enum CardVariant {
+    outlined = 'outlined',
+    primary = 'primary'
+}
 
 interface CardProps {
     width?: string;
     height?: string;
+    variant: CardVariant;
+    onClick: (num: number) => void
 }
 
-const Card = ({width, height}: CardProps) => {
-    return (
-        <div style={{width,height, background: 'grey'}}>
+const Card: React.FC<CardProps> = ({
+     width,
+     height,
+     variant,
+     onClick,
+     children,
 
+}) => {
+    const [state, setState] = useState(0)
+
+    return (
+        <div style={{width,height,
+            border: variant === CardVariant.outlined ? '1px solid grey' : 'none',
+            background: variant === CardVariant.primary ? 'lightgray' : '',
+        }}
+        onClick={() => onClick(state)}
+        >
+            {children}
         </div>
     );
 };
