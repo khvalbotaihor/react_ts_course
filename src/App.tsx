303d0a5e4714e64cda11d3dfc5ea8cedf060f98c
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-04 16:52:21 +0300

    - add navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import {BrowserRouter, Route, NavLink} from 'react-router-dom'
 import TodosPage from './components/TodosPage';
 import UsersPage from './components/UsersPage';
+import UserItemPage from "./components/UserItemPage";
+import TodoItemPage from "./components/TodoItemPage";
 
 const App = () => {
     return (
         <BrowserRouter>
             <div>
-            <div>
-                <NavLink to="/users">Users</NavLink>
-                <NavLink to="/todos">Todos</NavLink>
-            </div>
-            <Route path={'/users'} exact>
-                <UsersPage />
-            </Route>
-            <Route path={'/todos'} exact>
-                <TodosPage />
-            </Route>
+                <div>
+                    <NavLink to="/users">Users</NavLink>
+                    <NavLink to="/todos">Todos</NavLink>
+                </div>
+                <Route path={'/users'} exact>
+                    <UsersPage/>
+                </Route>
+                <Route path={'/todos'} exact>
+                    <TodosPage/>
+                </Route>
+                <Route path={'/user/:id'}>
+                    <UserItemPage />
+                </Route>
+                <Route path={'/todos/:id'}>
+                    <TodoItemPage />
+                </Route>
             </div>
         </BrowserRouter>
     );
---
+++ src/components/UserItemPage.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const UserItemPage:FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default UserItemPage;
