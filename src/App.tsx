f4b0836baaad31d695cbfeba7b2eccbabb2cb285
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-04 16:38:34 +0300

    - add browsder router

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,17 @@
-import React, {useEffect, useState} from 'react';
-import Card, {CardVariant} from "./components/Card";
-import UserList from "./components/UserList";
-import {ITodo, IUser} from "./types/types";
-import axios from "axios";
-import List from "./components/List";
-import UserItem from "./components/UserItem";
-import TodoItem from "./components/TodoItem";
-import EventsExample from "./components/EventsExample";
+import {BrowserRouter, Route} from 'react-router-dom'
+import TodosPage from './components/TodosPage';
+import UsersPage from './components/UsersPage';
 
 const App = () => {
-const [users, setUsers] = useState<IUser[]>([])
-const [todos, setTodos] = useState<ITodo[]>([])
-
-    useEffect(() => {
-        fetchUsers();
-        fetchTodos();
-    },[])
-
-    async function fetchUsers() {
-    try {
-        const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
-        setUsers(response.data)
-    }catch (e){
-        alert()
-    }
-    }
-
-    async function fetchTodos() {
-        try {
-            const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
-            setTodos(response.data)
-        }catch (e){
-            alert()
-        }
-    }
-
     return (
-        <div>
-            <EventsExample />
-            <Card width='200px' height="200px" variant={CardVariant.primary}>
-                <button>Button</button>
-            </Card>
-            <List
-                items={users}
-                renderItem={(user: IUser) => <UserItem user={user} key={user.id} /> }
-            />
-            <List
-                items={todos}
-                renderItem={(todo: ITodo) =>  <TodoItem todo={todo} key={todo.id} /> }
-            />
-        </div>
+        <BrowserRouter>
+            <Route path={'/users'} exact>
+                <UsersPage />
+            </Route>
+            <Route path={'/todos'} exact>
+                <TodosPage />
+            </Route>
+        </BrowserRouter>
     );
 };
 
