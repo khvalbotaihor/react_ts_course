8ecbf42df50c3b46e0df2aba5f482074d9be5ed7
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-04 16:49:33 +0300

    - add navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, NavLink} from 'react-router-dom'
 import TodosPage from './components/TodosPage';
 import UsersPage from './components/UsersPage';
 
 const App = () => {
     return (
         <BrowserRouter>
+            <div>
+            <div>
+                <NavLink to="/users">Users</NavLink>
+                <NavLink to="/todos">Todos</NavLink>
+            </div>
             <Route path={'/users'} exact>
                 <UsersPage />
             </Route>
             <Route path={'/todos'} exact>
                 <TodosPage />
             </Route>
+            </div>
         </BrowserRouter>
     );
 };
